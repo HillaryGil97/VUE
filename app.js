2d86bc0d163c4c6eaac766ff84e3f8943d842541
HillaryGil <hillarygil97@gmail.com>
2022-04-18 21:23:17 -0500

    Expresiones

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 //console.log(Vue) //demostrando que Vue es un objeto muy grande de recusoss
 
 const app = Vue.createApp({
-    template: `
-        <h1>Hola mundo </h1>
-        <p> Desde app.js </p>
-    `
+    //template: `
+        //<h1>Hola mundo </h1>
+        //<p>{{[1,2,3,4]}}</p> 
+        //<p>Desde app.js</p>
+    //`
+    
 })
 
 app.mount('#myApp') //# referencia a un ID como en CSS
